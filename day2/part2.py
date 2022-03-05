15a517482f5f95373719aa673c5bb4f9ad771d7f
Danya <90642567+xXxXxXxXxXxXxXxXxDanyaxXxXxXxXxXxXxXxXx@users.noreply.github.com>
2022-03-06 01:46:24 +0700

    Create part2.py

---
+++ day2/part2.py
@@ -0,0 +1,33 @@
+#Part2
+with open('input.txt') as f:
+    list = f.readlines()
+    AllLength = 0
+    for i in range(len(list)):
+        list2 = list[i].split('x')
+        l = int(list2[0])
+        w = int(list2[1])
+        h = int(list2[2])
+        list3 = []
+        list3.append(l)
+        list3.append(w)
+        list3.append(h)
+        if list3[0] < list3[1]:
+            if list3[1] < list3[2]:
+                min1 = list3[0]
+                min2 = list3[1]
+            else:
+                min1 =list3[0]
+                min2 = list3[2]
+        else:
+            if list3[0] < list3[2]:
+                min1 = list3[0]
+                min2 = list3[1]
+            else:
+                min1 = list3[1]
+                min2 = list3[2]
+
+        atribut = l * w * h + 2 * (min1 + min2)
+        AllLength += atribut
+print(AllLength)
+with open('output1.txt','w') as h:
+    h.write(str(AllLength))
