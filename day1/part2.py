0060b8f2f5f97438df7b84e50f00388215257af2
Danya <90642567+xXxXxXxXxXxXxXxXxDanyaxXxXxXxXxXxXxXxXx@users.noreply.github.com>
2022-03-05 20:39:54 +0700

    Update part2.py

--- day1/part2.py
+++ day1/part2.py
@@ -1,1 +1,14 @@
-
+# АоС_1
+with open('input.txt') as f:
+    up = 0
+    down = 0
+    for row in f:
+        for letter in row:
+            if letter == '(':
+                up += 1
+            if letter == ')':
+                down += 1
+            if (down - up) == 1:
+                with open('output.txt','w') as h:
+                    h.write(str(down + up))
+                    break
