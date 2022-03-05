ebb75b836785c7429e33a2e3ed3555c5428d3af4
Danya <90642567+xXxXxXxXxXxXxXxXxDanyaxXxXxXxXxXxXxXxXx@users.noreply.github.com>
2022-03-05 17:12:36 +0700

    Update part1.py

--- 1 день/part1.py
+++ 1 день/part1.py
@@ -2,15 +2,12 @@
 with open('input.txt') as f:
     up = 0
     down = 0
-    letter2 = 0
     for row in f:
         for letter in row:
             if letter == '(':
                 up += 1
-                print(letter,up)
             if letter == ')':
                 down += 1
-                print(letter,down)
 result = up - down
 with open('output.txt','w') as h:
     h.write(str(result))
