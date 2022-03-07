3fe7bd70abeaeb8357ec8942fcb49f22e1e78559
Danya <90642567+xXxXxXxXxXxXxXxXxDanyaxXxXxXxXxXxXxXxXx@users.noreply.github.com>
2022-03-07 21:17:31 +0700

    Update part1.py

--- day3/part1.py
+++ day3/part1.py
@@ -2,6 +2,10 @@
 with open('input.txt') as f:
     list = f.read()
     big_list = []
+    list_mini = []
+    list_mini.append(0)
+    list_mini.append(0)
+    big_list.append(list_mini)
     x = 0
     y = 0
     for i in range(len(list)):
@@ -18,6 +22,6 @@ with open('input.txt') as f:
         list_mini.append(y)
         if big_list.count(list_mini) == 0:
             big_list.append(list_mini)
-    housesWithGifts = len(big_list) + 1
+    housesWithGifts = len(big_list)
 with open('output1.txt','w') as h:
     h.write(str(housesWithGifts))
