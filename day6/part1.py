59653de8a4cdc770324a1ebcc2b432279790a953
Danya <90642567+xXxXxXxXxXxXxXxXxDanyaxXxXxXxXxXxXxXxXx@users.noreply.github.com>
2022-03-13 19:54:17 +0700

    Create part1.py

---
+++ day6/part1.py
@@ -0,0 +1,43 @@
+#Part_1 
+with open('input.txt') as f:
+    list = f.readlines()
+    on = set()
+    for i in range(len(list)):
+        line = list[i]
+        data = line.split()
+        if data[0] == 'turn':
+            element = data[2].replace(',',' ').split()
+            element_2 = data[4].replace(',',' ').split()
+            for i in range(len(element)):
+                element.insert(i,int(element[i]))
+                del element[i+1]
+                element_2.insert(i,int(element_2[i]))
+                del element_2[i+1]
+            if data[1] == 'on':
+                for y in range(element_2[1]-element[1]):
+                    for x in range(element_2[0]-element[0]):
+                        if (element[0]+x,element[1]+y) not in on:
+                            on.add((element[0]+x,element[1]+y))
+                            #print((element[0]+x,element[1]+y))
+            else:
+                for y in range(element_2[1]-element[1]):
+                    for x in range(element_2[0]-element[0]):
+                        on.discard((element[0]+x,element[1]+y))
+                            
+        else:
+            element = data[1].replace(',',' ').split()
+            element_2 = data[3].replace(',',' ').split()
+            for i in range(len(element)):
+                element.insert(i,int(element[i]))
+                del element[i+1]
+                element_2.insert(i,int(element_2[i]))
+                del element_2[i+1]
+            for y in range(element_2[1]-element[1]):
+                    for x in range(element_2[0]-element[0]):
+                        if (element[0]+x,element[1]+y) not in on:
+                            on.add((element[0]+x,element[1]+y))
+                        else:
+                            on.discard((element[0]+x,element[1]+y))
+print(len(on))
+with open('output1.txt','w') as h:
+    h.write(len(on))
