ddc420681f60a9f87aef9e42255968676ffa2dfa
Danya <90642567+xXxXxXxXxXxXxXxXxDanyaxXxXxXxXxXxXxXxXx@users.noreply.github.com>
2022-04-01 10:50:32 +0700

    Create part1.py

---
+++ day10/part1.py
@@ -0,0 +1,24 @@
+# Part_1
+with open('input.txt') as f:
+    sequence = f.read()
+    count = 0 
+    while count < 40:
+        instances = 1
+        sequence_new = ''
+        for i in range(len(sequence)-1):
+            if sequence[i] == sequence[i+1]:
+                instances += 1
+            else:
+                sequence_new = sequence_new + str(instances)
+                sequence_new = sequence_new + str(sequence[i])
+                instances = 1
+        if sequence[i] != sequence[i+1]:
+            sequence_new = sequence_new + '1'
+            sequence_new = sequence_new + str(sequence[i+1])
+        else:
+            sequence_new = sequence_new + str(instances)
+            sequence_new = sequence_new + str(sequence[i])
+        count += 1
+        sequence = sequence_new
+with open('output1.txt','w') as h:
+    h.write(str(len(sequence)))
