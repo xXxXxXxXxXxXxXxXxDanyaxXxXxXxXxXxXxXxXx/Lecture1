5283bde9448cfccbaf794a284cfbd4ad5951ef5f
Danya <90642567+xXxXxXxXxXxXxXxXxDanyaxXxXxXxXxXxXxXxXx@users.noreply.github.com>
2022-04-01 11:17:16 +0700

    Update part1.py

--- day6/part1.py
+++ day6/part1.py
@@ -9,35 +9,39 @@ with open('input.txt') as f:
             element = data[2].replace(',',' ').split()
             element_2 = data[4].replace(',',' ').split()
             for i in range(len(element)):
-                element.insert(i,int(element[i]))
-                del element[i+1]
-                element_2.insert(i,int(element_2[i]))
-                del element_2[i+1]
+                # element.insert(i,int(element[i]))
+                # del element[i+1]
+                element[i] = int(element[i])
+                # element_2.insert(i,int(element_2[i]))
+                # del element_2[i+1]
+                element_2[i] = int(element_2[i])
             if data[1] == 'on':
-                for y in range(element_2[1]-element[1]):
-                    for x in range(element_2[0]-element[0]):
+                for y in range(element_2[1]-element[1] + 1):
+                    for x in range(element_2[0]-element[0] + 1):
                         if (element[0]+x,element[1]+y) not in on:
                             on.add((element[0]+x,element[1]+y))
                             #print((element[0]+x,element[1]+y))
             else:
-                for y in range(element_2[1]-element[1]):
-                    for x in range(element_2[0]-element[0]):
+                for y in range(element_2[1]-element[1] + 1):
+                    for x in range(element_2[0]-element[0] + 1):
                         on.discard((element[0]+x,element[1]+y))
                             
         else:
             element = data[1].replace(',',' ').split()
             element_2 = data[3].replace(',',' ').split()
             for i in range(len(element)):
-                element.insert(i,int(element[i]))
-                del element[i+1]
-                element_2.insert(i,int(element_2[i]))
-                del element_2[i+1]
-            for y in range(element_2[1]-element[1]):
-                    for x in range(element_2[0]-element[0]):
+                # element.insert(i,int(element[i]))
+                # del element[i+1]
+                element[i] = int(element[i])
+                # element_2.insert(i,int(element_2[i]))
+                # del element_2[i+1]
+                element_2[i] = int(element_2[i])
+            for y in range(element_2[1]-element[1] + 1):
+                    for x in range(element_2[0]-element[0] + 1):
                         if (element[0]+x,element[1]+y) not in on:
                             on.add((element[0]+x,element[1]+y))
                         else:
                             on.discard((element[0]+x,element[1]+y))
 print(len(on))
 with open('output1.txt','w') as h:
-    h.write(len(on))
+    h.write(str(len(on)))
