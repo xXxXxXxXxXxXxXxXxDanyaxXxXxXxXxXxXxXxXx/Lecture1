047e49242d9da1f064b1e5abf98156c30e3dc445
Danya <90642567+xXxXxXxXxXxXxXxXxDanyaxXxXxXxXxXxXxXxXx@users.noreply.github.com>
2022-04-07 17:21:37 +0700

    Update part1.py

--- day7/part1.py
+++ day7/part1.py
@@ -2,7 +2,7 @@
 with open('input.txt') as f:
     list = f.readlines()
     dictionary = {}
-    print('длинна = ',len(list))
+    
     while list:
         
         del_elements = []
@@ -10,25 +10,63 @@ with open('input.txt') as f:
         for i in range(len(list)):
             line = list[i]
             data = line.split()
+            print(data)
 
             if len(data) == 3 and data[0].isdigit():
-                dictionary[data[2]] = data[0]
+                dictionary[data[2]] = int(data[0])
                 del_elements.append(list[i])
+                print(dictionary[data[2]])
+                
+            elif len(data) == 3 and data[0] in dictionary:
+                dictionary[data[2]] = dictionary[data[0]]
+                del_elements.append(list[i])
+                dictionary[data[2]]
                 
             elif len(data) == 4 and data[1] in dictionary:
-                dictionary[data[3]] = ~ data[1]
+                dictionary[data[3]] = ~ dictionary[data[1]]
                 del_elements.append(list[i])
+                dictionary[data[3]]
                 
             else: # len(data) == 5:
-                if data[1] == 'AND' and data[0] in dictionary and data[2] in dictionary:
-                    dictionary[data[4]] = data[0] & data[2]
-                elif data == 'OR' and data[0] in dictionary and data[2] in dictionary:
-                    dictionary[data[4]] = data[0] | data[2]
-                elif data == 'RSHIFT' and data[0 in dictionary and data[2] in dictionary:
-                    dictionary[data[4]] = data[0] >> data[2]
-                else:
-                    dictionary[data[4]] = data[0] << data[2]
-                del_elements.append(list[i])
-                                               
+                
+                if data[1] == 'AND':
+                    
+                    if data[0] in dictionary and data[2] in dictionary:
+                        dictionary[data[4]] = dictionary[data[0]] & dictionary[data[2]]
+                        del_elements.append(list[i])
+                        dictionary[data[4]]
+                        
+                    elif data[0].isdigit() and data[2] in dictionary:
+                        dictionary[data[4]] = int(data[0]) & dictionary[data[2]]
+                        del_elements.append(list[i])
+                        dictionary[data[4]]
+                        
+                elif data[1] == 'OR':
+                    
+                    if data[0] in dictionary and data[2] in dictionary:
+                        dictionary[data[4]] = dictionary[data[0]] | dictionary[data[2]]
+                        del_elements.append(list[i])
+                        dictionary[data[4]]
+                        
+                    elif data[0].isdigit() and data[2] in dictionary:
+                        dictionary[data[4]] = int(data[0]) | dictionary[data[2]]
+                        del_elements.append(list[i])
+                        dictionary[data[4]]
+                        
+                elif data[1] == 'RSHIFT' and data[0] in dictionary:
+                    dictionary[data[4]] = dictionary[data[0]] >> int(data[2])
+                    del_elements.append(list[i])
+                    dictionary[data[4]]
+                    
+                elif data[1] == 'LSHIFT' and data[0] in dictionary:
+                    dictionary[data[4]] = dictionary[data[0]] << int(data[2])
+                    del_elements.append(list[i])
+                    dictionary[data[4]]
+        print('___________')
+        print(dictionary)
+        
         for i in del_elements:
             list.remove(i)
+            
+with open('output1.txt','w') as h:
+    h.write(str(dictionary['a']))
